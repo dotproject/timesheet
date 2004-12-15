6efe2d8295c6de40c21e0c45a1ac25a546280eaa
jcgonz <jcgonz>
2004-12-15 23:34:46 +0000

    Updated scripts so they use AppUI->getConfig wich is the correct way nowadays. The prior funcion was depreciated.

--- vw_tasks_todo.php
+++ vw_tasks_todo.php
@@ -2,6 +2,6 @@
 global $m, $a;
 
 $min_view = 1;
-include_once $AppUI->getConfig( 'root_dir' ).'/modules/tasks/tasks.class.php';
-include $AppUI->getConfig( 'root_dir' ).'/modules/tasks/todo.php';
+include_once dPgetConfig( 'root_dir' ).'/modules/tasks/tasks.class.php';
+include dPgetConfig( 'root_dir' ).'/modules/tasks/todo.php';
 ?>
